8f02764e2fb81ffae9c8fb8a76bf153e014c8d50
Svatosik <125908175+Svatosik@users.noreply.github.com>
2023-08-05 18:04:50 +0000

    Deploying to gh-pages from @ Svatosik/goit-js-hw-09@77cbdb84e9b09836cb7faa6f95ab457563d86d75 🚀

---
+++ 01-color-switcher.f0dd2790.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;function a(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}t.addEventListener("click",(function(){n||(t.disabled=!0,e.disabled=!1,n=setInterval(a,1e3))})),e.addEventListener("click",(function(){n&&(t.disabled=!1,e.disabled=!0,clearInterval(n),n=null)}))}();
+//# sourceMappingURL=01-color-switcher.f0dd2790.js.map
